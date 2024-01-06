29015c869a8e2a117c16f64c6a1a4a9f6f490ec7
endofheat <endofheat97@gmail.com>
2024-01-07 04:56:11 +1300

    add and update require, sync part

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,14 +1,31 @@
 "use strict";
-const user = require("./user");
-const item = require("./item");
+// require models
+const CategoryTag = require('./categoryTag');
+const Fridge = require('./fridge');
+const FridgeItem = require('./fridgeItem');
+const Item = require('./item');
+const ItemTag = require('./itemTag');
+const ShoppingList = require('./shoppingList');
+const User = require('./user');
 
+//sync models
 async function init() {
-    await user.sync();
+    await CategoryTag.sync();
+    await Fridge.sync();
+    await FridgeItem.sync();
+    await Item.sync();
+    await ItemTag.sync();
+    await ShoppingList.sync();
+    await User.sync();
 }
 init();
 
 module.exports = {
-    user,
-    item,
-
-}
+    CategoryTag,
+    Fridge,
+    FridgeItem,
+    Item,
+    ItemTag,
+    ShoppingList,
+    User,
+};

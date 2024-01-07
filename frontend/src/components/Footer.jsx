c9c4402c574f90547f8a8e82aa02171a1d86e134
endofheat <endofheat97@gmail.com>
2024-01-08 06:56:54 +1300

    initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer() {
+    return (
+        <div className="Footer">9 Oct 2023 SE Cohort IoD Capstone Project &copy; Copyright 2023 Made with ❤ by Akira </div>
+    )
+}
+
+export default Footer;

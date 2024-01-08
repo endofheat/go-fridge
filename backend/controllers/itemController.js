97656f30ca7c7d543fe7fe6d728d0710ef441019
endofheat <endofheat97@gmail.com>
2024-01-09 10:14:24 +1300

    Merge code from userController

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -30,21 +30,68 @@ const getItemByName = (req, res) => {
     })
 }
 
-const getItemByTag = (req, res) => {
-    let tagName = req.query.text;
-    Models.Item.find({})
-    .then((data) => {
-        let tagMatchedItems = data.data.filter(
+// const getItemByTag = (req, res) => {
+//     let tagName = req.query.text;
+//     Models.Item.find({})
+//     .then((data) => {
+//         let tagMatchedItems = data.data.filter(
 
-            )
-        res.send({ result: 200, data: tagMatchedItems })
-    })
-}
+//             )
+//         res.send({ result: 200, data: tagMatchedItems })
+//     })
+// }
 
+const registerItem = async (req, res) => {
+    try {
+        const { ItemName, email, password } = req.body;
+        // Validate Item input
+        if (!(email && password && ItemName)) {
+            res.status(400).json({ result: "All input is required" }); // code 400, 'Bad Request'
+        return; // when sending responses and finishing early, manually return or end the function to stop further processing
+    }
 
+    // Validate if Item exists in database
+    const oldItem = await Models.Item.findOne({ where: { email } });
+
+    if (oldItem) {
+        res.status(409).json({ result: "Item already exists. Please login" }); // code 409, 'Conflict'
+        return; // when sending responses and finishing early, manually return or end the function to stop further processing
+    }
+    // Verify email using Kickbox
+    const kickboxResponse = await kickbox.verify(email);
+
+    if (!kickboxResponse.success || kickboxResponse.result === "undeliverable" || kickboxResponse.disposable) {
+        res.status(400).json({ result: "Invalid email address" });
+        return;
+    }
+
+    // Encrypt Item password
+    let encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create Item in database
+    const ItemMetadata = await Models.Item.create({
+        itemName,
+        quantity, // sanitize: convert email to lowercase
+        password: encryptedPassword,
+    });
+    const Item = ItemMetadata.get({ plain: true }); // get just the Item fields, no extra sequelize metadata
+
+    // Create token
+    const token = createToken(Item.id, email);
+
+    // save Item token to send back to front-end
+    Item.token = token;
+
+    // return new Item
+    res.status(201).json({ result: "Item successfully registered", data: Item });
+    } catch (err) {
+    console.log(err);
+    res.status(500).json({ result: err.message });
+    }
+};
 
 module.exports = {
     getItems,
     getItemByName,
-    getItemByTag,
+    // getItemByTag,
 }

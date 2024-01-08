5f0bdbe8db0ca1da3f5c51465020d87408dc260b
endofheat <endofheat97@gmail.com>
2024-01-09 10:24:39 +1300

    update go back redirect format

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, Outlet } from "react-router-dom";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function PageNotFound() {
     const navigate = useNavigate();
@@ -10,9 +10,9 @@ function PageNotFound() {
       <h1>404</h1>
       <h2>Page Not Found</h2>
       <p>
-        What were you looking for? Come on, people, you gotta go {/* <Link to="/"> */}home{/* </Link> */}. 
+        What were you looking for? Come on, people, you gotta go<Link to="/"> home </Link>. 
       </p>
-      Okay, <button onClick={() => navigate(-1)}>go back</button> by the bus and wait for me, all right? Go back and wait. 
+      Okay, <Link to="#" onClick={() => navigate(-1)}>go back</Link> by the bus and wait for me, all right? Go back and wait. 
     </div>
   );
 }

095995d9fcde5d86b9e4577eee6faeddff5686bb
endofheat <endofheat97@gmail.com>
2024-01-09 12:28:00 +1300

    update comment

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,24 +1,29 @@
-const express = require("express");
-const router = express.Router();
-const Controllers = require("../controllers/userController");
-const { verifyToken } = require("../middleware/auth");
+let express = require("express");
+let router = express.Router();
+let Controllers = require("../controllers/userController");
+let { verifyToken } = require("../middleware/auth");
 
+// GET all users
 router.get('/', verifyToken, (req, res) => { // uses authentication middleware function to verify the user token before controller function runs
     Controllers.getUsers(res);
 })
 
+// POST login user
 router.post('/login', (req, res) => {
     Controllers.loginUser(req, res)
 })
 
+// POST new user sign up
 router.post('/register', (req, res) => {
-    Controllers.registerUser(req, res)
+    Controllers.registerUser(req.body, res)
 })
 
+// PUT update user info
 router.put('/:id', (req, res) => {
     Controllers.updateUser(req, res)
 })
 
+// DELETE delete exited user
 router.delete('/:id', (req, res) => {
     Controllers.deleteUser(req, res)
 })

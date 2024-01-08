92f531cfca2a485e7a9d443ccdadd372c03ebc0d
endofheat <endofheat97@gmail.com>
2024-01-08 16:46:16 +1300

    Update models to match database design

--- backend/models/item.js
+++ backend/models/item.js
@@ -2,11 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
-    itemName: { type: String, trim: true, required: true, unique: true },
-    quantity: { type: Number, },
-    email: { type: String, trim: true, required: true, unique: true },
-    password: { type: String, trim: true, required: true, unique: true },
-    fridgeID: { type: Schema.Types.ObjectId, ref: "fridge" },
+    itemName: { type: String, required: true, unique: true },
+    quantity: { type: Number },
+    unit: { type: String, required: true},
+    expireDate: { type: Date },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
 });
--- backend/models/shoppingList.js
+++ backend/models/shoppingList.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const shoppingListSchema = new Schema({
-    listName: { type: String, trim: true, required: true, unique: true },
+    listName: { type: String, required: true, unique: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
     userID: { type: Schema.Types.ObjectId, ref: "user" },

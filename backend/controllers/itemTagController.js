7029859b18eb5df706dc5f7effbd798cd85fca27
endofheat <endofheat97@gmail.com>
2024-01-09 18:26:27 +1300

    add a separate get function

--- backend/controllers/itemTagController.js
+++ backend/controllers/itemTagController.js
@@ -1,6 +1,17 @@
 'use strict';
-
 let Models = require('../models');
+
+const getItemTags = (res) => {
+    // get all items from database
+    Models.ItemTag.find({})
+    .then((data) => {
+        res.send({result: 200, data: data })
+    })
+    .catch((err) => {
+        console.log(err);
+        res.send({ result: 500, data: err.message }); // code 500, 'Internal Server Error'
+    })
+}
 
 const getAllItemTags = (res) => {
     // get all items assigned for tag
@@ -50,6 +61,7 @@ const deleteItemTag = (req, res) => {
 }
 
 module.exports = {
+    getItemTags,
     getAllItemTags,
     createItemTag,
     updateItemTag,

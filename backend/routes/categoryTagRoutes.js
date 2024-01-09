6fe3b4cdaa171cf9d9a82902737f6c79fbce5aed
endofheat <endofheat97@gmail.com>
2024-01-09 14:22:09 +1300

    initial commit

---
+++ backend/routes/categoryTagRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const Controllers = require("../controllers/categoryTagController");
+
+// GET all tags
+router.get('/', (req, res) => {
+    Controllers.getTags(res);
+});
+
+// GET tag by name
+router.get('/', (req, res) => {
+    Controllers.getTagByName(req, res);
+});
+
+// POST add a new tag
+router.post('/create', (req, res) => {
+    Controllers.addTag(req.body, res);
+});
+
+// PUT update a tag
+router.put('/:id', (req, res) => {
+    Controllers.updateTag(req, res);
+});
+
+// DELETE delete existed tag
+router.delete('/:id', (req, res) => {
+    Controllers.deleteTag(req, res);
+});
+
+module.exports = router;

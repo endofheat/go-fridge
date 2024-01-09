d89840c7edf98672bd803dbf62e9f06e4897f6ec
endofheat <endofheat97@gmail.com>
2024-01-09 15:51:43 +1300

    update route based on controller

---
+++ backend/routes/itemTagRoutes.js
@@ -0,0 +1,26 @@
+let express = require("express");
+let router = express.Router();
+let Controllers = require("../controllers/itemTagController");
+
+// GET items assigned to the ItemTag list
+router.get('/item/:id', (req, res) => {
+    Controllers.getAllItemTags(req, res);
+});
+
+// POST add new ItemTag list
+router.post('/create', (req, res) => {
+    Controllers.createItemTag(req, res);
+});
+
+// PUT update ridgeItem list
+router.put('/:id', (req, res) => {
+    Controllers.updateItemTag(req, res);
+});
+
+// DELETE delete existed ItemTag list
+router.delete('/:id', (req, res) => {
+    Controllers.deleteItemTag(req, res);
+});
+
+
+module.exports = router;

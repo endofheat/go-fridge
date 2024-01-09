75d20812a504017c743b962a2ef0a00e6462e521
endofheat <endofheat97@gmail.com>
2024-01-09 15:11:43 +1300

    initial commit

---
+++ backend/controllers/fridgeController.js
@@ -0,0 +1,69 @@
+"use strict";
+let Models = require("../models");
+
+const getFridges = (res) => {
+    // get all fridges from database
+    Models.Fridge.find({})
+    .then((data) => {
+        res.send({result: 200, data: data })
+    })
+    .catch((err) => {
+        console.log(err);
+        res.send({ result: 500, data: err.message }); // code 500, 'Internal Server Error'
+    })
+}
+
+const getFridgeItem = (req, res) => {
+    // finds all items for a given fridge and populates with details
+    Models.Fridge.find({ fridgeItemID: req.params.id })
+    .populate({path:'fridgeItemID'})
+    .then((data) => {
+        res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+    console.log(err);
+        res.send({ result: 500, data: err.message });
+    });
+};
+
+const createFridge = (data, res) => {
+    new Models.Fridge(data)
+    .save()
+    .then((data) => {
+        res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+    console.log(err);
+        res.send({ result: 500, data: err.message });
+    });
+}
+
+const updateFridge = (req, res) => {
+    Models.Fridge.findByIdAndUpdate(req.params.id, req.body.id, { new: true })
+    .then((data) => {
+        res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+    console.log(err);
+        res.send({ result: 500, data: err.message });
+    });
+}
+
+const deleteFridge = (req, res) => {
+    Models.Fridge.findByIdAndDelete(req.params.id)
+    .then((data) => {
+        res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+    console.log(err);
+        res.send({ result: 500, data: err.message });
+    });
+}
+
+module.exports = {
+    getFridges,
+    getFridgeItem,
+    createFridge,
+    updateFridge,
+    deleteFridge,
+};

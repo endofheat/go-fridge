ea6fce59eed72414483378aa41e93aacf6185171
endofheat <endofheat97@gmail.com>
2024-01-11 01:59:15 +1300

    drop unnecessary tables

--- backend/models/fridge.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const fridgeSchema = new Schema({
-    fridgeItemID: { type: Schema.Types.ObjectId, ref: "fridgeItem" },
-});
-
-module.exports = mongoose.model("fridge", fridgeSchema);

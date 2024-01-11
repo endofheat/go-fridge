5fce9769dae732f272340133db90974983379fb9
endofheat <endofheat97@gmail.com>
2024-01-11 16:53:39 +1300

    remove unused code

--- backend/models/index.js
+++ backend/models/index.js
@@ -7,18 +7,6 @@ const ItemTag = require('./itemTag');
 const ShoppingList = require('./shoppingList');
 const User = require('./user');
 
-//sync models
-// async function init() {
-//     await Tag.sync();
-//     await Fridge.sync();
-//     await FridgeItem.sync();
-//     await Item.sync();
-//     await ItemTag.sync();
-//     await ShoppingList.sync();
-//     await User.sync();
-// }
-// init();
-
 module.exports = {
     Tag,
     FridgeItem,

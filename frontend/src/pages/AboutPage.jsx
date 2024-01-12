150854860298e3a3c583e259083e1f7cb1e0a49a
endofheat <endofheat97@gmail.com>
2024-01-12 14:02:54 +1300

    update <p> to responsive one

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,10 +1,17 @@
+import { ThemeProvider, Typography } from "@mui/material";
+import { ResponsiveFont } from "../themes/responsiveFontTheme";
+
 export default function AboutPage() {
     // Save in pages/AboutPage.jsx
     return (
       <div className="About">
-        <h1>About</h1>
-        <p>This is Akira Li's capstone project for Institute of Data & AUT Software Engineering Programme, for study use only.</p> 
-        <p>You're welcome to download the code and play with it. </p>
+        <ThemeProvider theme={ResponsiveFont}>
+
+        <Typography variant="h1">About</Typography>
+        <Typography variant="p">This is Akira Li's capstone project for Institute of Data & AUT Software Engineering Programme, for study use only.</Typography>
+        <p></p>
+        <Typography variant="p">You're welcome to download the code and play with it. </Typography>
+        </ThemeProvider>
       </div>
     );
   }
